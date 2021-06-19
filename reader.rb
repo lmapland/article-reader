ef442808f68543e116cafdbb65b28976394f51b2
Leslie Apland <leslie.apland@gmail.com>
2021-06-19 10:57:03 -0500

    Adds ability to pass in filename
    Adds ability for user to choose the filename
     and makes the filename safe for use
    If no filename is passed in, creates a unique filename

--- reader.rb
+++ reader.rb
@@ -1,6 +1,7 @@
 require 'optparse'
 require 'net/http'
 require 'uri'
+require 'time'
 
 # Set up options
 options = {}
@@ -9,6 +10,10 @@ option_parser = OptionParser.new do |opts|
   opts.banner = "Usage: ruby ./reader.rb [-a|--article] <url>"
   opts.on("-a", "--article url", "An article url is required.") do |a|
     options[:article] = a
+  end
+
+  opts.on("-n", "--name filename", "The file name of the output file.") do |n|
+    options[:name] = n
   end
 end
 
@@ -23,8 +28,20 @@ end
 # Get the article
 res = Net::HTTP.get URI(options[:article])
 
+# Choose a file name to write the file to
+# Turn an invalid filename into a valid one
+fn = options[:name]
+if fn.nil?
+  fn = Time.new.to_i
+else
+  fn.gsub!(/[^0-9A-Za-z.\-]/, '_')
+end
+
+fn << ".txt"
+
 # Write article to file
-# Todo: make filename dynamic / give ability to pass it in
-f = File.open("output/article.txt", "w")
+f = File.open("output/#{fn}", "w")
 f.write(res)
 f.close
+
+puts "URL data has been written to #{fn}"
